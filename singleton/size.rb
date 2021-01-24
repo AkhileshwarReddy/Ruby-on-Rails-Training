2e64f65cde202c89d5971c3e568cb8b055bdb48a
Akhil Ankireddy <akhil.vedas@outlook.com>
2021-01-24 16:55:47 +0530

    Add size.rb in singleton

---
+++ singleton/size.rb
@@ -0,0 +1,15 @@
+# Create a singleton method to change the size method defination for a particular object
+
+birds = ["Duck", "Dove", "Sparrow"]
+animals = ["Lion", "Tiger", "Dog", "Cat"]
+
+def birds.size
+    return "0"
+end
+
+puts "birds: #{birds}"
+puts "birds size: #{birds.size}"
+
+puts "animals: #{animals}"
+puts "animals size: #{animals.size}"
+
